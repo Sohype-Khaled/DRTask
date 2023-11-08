27f3445c12b382dbb0d2454930a1c25d6f2344d5
Sohype-Khaled <sohype.mop@gmail.com>
2023-11-08 02:06:08 +0200

    wip user api register

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SaveUserRequest;
+use App\Models\Shop;
 use App\Models\User;
 use App\Traits\HTTPResponses;
 use Illuminate\Http\Request;
@@ -17,14 +18,28 @@ class AuthController extends Controller
 
     }
 
-    public function register(SaveUserRequest $request)
+    public function register(SaveUserRequest $request, Shop $shop)
     {
-        $request->validated($request->all());
-        $user = User::create($request->all());
+        $user = User::where('email', $request->input('email'));
 
+        if ($user->exists()) {
+            $user = $user->first();
+            if (!$shop->members()->where('user_id', $user->id)->exists())
+                $shop->members()->attach($user->id);
+            else
+                return $this->error('User already exists', 400);
+
+        } else {
+            $request->validated($request->all());
+
+            $user = User::create($request->all());
+
+            $shop->members()->attach($user->id);
+
+        }
         return $this->success([
             'user' => $user,
-            'token' => $user->createToken("{shop:slug}-{user:name}-token")->plainTextToken
+            'token' => $user->createToken("{$shop->slug}-{$user->email}-token")->plainTextToken
         ], 'User created successfully', 201);
     }
 
